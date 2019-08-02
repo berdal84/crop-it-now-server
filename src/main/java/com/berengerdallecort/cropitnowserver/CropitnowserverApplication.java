d7db80f94d66b0fa1566551ed987028595b0714d
Bérenger Dalle-Cort <contact@dalle-cort.fr>
2019-08-02 11:30:33 +0200

    First commit with default generated files by SpringInitializr.
    Uses apache-maven-3.6.1-bin and jre-8u221-x64

---
+++ src/main/java/com/berengerdallecort/cropitnowserver/CropitnowserverApplication.java
@@ -0,0 +1,13 @@
+package com.berengerdallecort.cropitnowserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropitnowserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropitnowserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berengerdallecort/cropitnowserver/CropitnowserverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.berengerdallecort.cropitnowserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CropitnowserverApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
